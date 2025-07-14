8173aa1fb20518dd370c462b0c91449e5bbdef9a
Patrick Kurtz <patrick.kurtz@gmail.com>
2025-07-14 10:57:16 -0500

    adding GPS

--- picarGPS.py
+++ picarGPS.py
@@ -1,36 +1,26 @@
 from gps import gps, WATCH_ENABLE
 import time
 
+# Start GPSD session
 session = gps(mode=WATCH_ENABLE)
 
-def latestLat():
-    try:
-        report = session.next()
-        if report['class'] == 'TPV':
-            myLat = getattr(report, 'lat', None)
-    return myLat
-
-def latestLon():
-    try:
-        report = session.next()
-        if report['class'] == 'TPV':
-            myLon = getattr(report, 'lat', None)
-    return myLon
-
-while True:
-    try:
+def get_latest_coords():
+    while True:
         report = session.next()
         if report['class'] == 'TPV':
             lat = getattr(report, 'lat', None)
             lon = getattr(report, 'lon', None)
-
             if lat is not None and lon is not None:
-                maps_url = f"https://www.google.com/maps?q={lat},{lon}"
-                print(f"Latitude: {lat}, Longitude: {lon}")
-                print(f"Google Maps: {maps_url}\n")
+                return lat, lon
 
-        time.sleep(1)
+#while True:
+#    try:
+        lat, lon = get_latest_coords()
+       # maps_url = f"https://www.google.com/maps?q={lat},{lon}"
+       # print(f"Latitude: {lat}, Longitude: {lon}")
+       # print(f"Google Maps: {maps_url}\n")
+       # time.sleep(1)
 
-    except KeyboardInterrupt:
-        print("Exiting...")
-        break
+  #  except KeyboardInterrupt:
+  #      print("Exiting...")
+  #      break

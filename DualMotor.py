42fe72a3512800d3eeb1949fadd74df2211cd94a
Patrick Kurtz <patrick.kurtz@gmail.com>
2025-07-30 15:21:46 -0500

    Added dual Motor

--- DualMotor.py
+++ DualMotor.py
@@ -14,15 +14,15 @@ motoRPin1 = DigitalOutputDevice(27)           # define L293D pin according to BC
 motoRPin2 = DigitalOutputDevice(17)           # define L293D pin according to BCM Numbering
 enablePin = PWMOutputDevice(22,frequency=1000)
 
-# mapNUM function: map the value from a range of mapping to another range.
+
 def mapNUM(value,fromLow,fromHigh,toLow,toHigh):
     return (toHigh-toLow)*(value-fromLow) / (fromHigh-fromLow) + toLow
 
 # motor function: determine the direction and speed of the motor according to the input ADC value input
 def motor(speed):
-
+    value = ADC -128
     if (speed > 0):  # make motor turn forward
-        motoRPin1.on()        # motoRPin1 output HIGH level
+        motoRPin1.on()        # motoRPin1 output HIHG level
         motoRPin2.off()       # motoRPin2 output LOW level
         print ('Turn Forward...')
     elif (speed < 0): # make motor turn backward
@@ -34,24 +34,30 @@ def motor(speed):
         motoRPin2.off()
         print ('Motor Stop...')
     b=mapNUM(abs(speed),0,128,0,100)
-    enablePin.value = b / 100.0     # set dc value as the duty cycle
+    enablePin.speed = b / 100.0     # set dc value as the duty cycle
     print ('The PWM duty cycle is %d%%\n'%(abs(speed)*100/127))   # print PMW duty cycle.
 
 def loop():
-    start = -50
-    while start <= 50:
-        print (start)
-        motor(start)
-        time.sleep(2)
-        start = start + 25
+    value = 1
+    while value < 102:
+        if(value > 102 )
+            False
+        print ('Speed : %d'%(value))
+        motor(value)
+        time.sleep(0.5)
+        value = value + 10
+
+
 
 def destroy():
     motoRPin1.close()
     motoRPin2.close()
     enablePin.close()
+    adc.close()
 
 if __name__ == '__main__':  # Program entrance
     print ('Program is starting ... ')
+    setup()
     try:
         loop()
     except KeyboardInterrupt: # Press ctrl-c to end the program.
